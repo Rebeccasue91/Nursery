e789bb495af529451b05b84dae61d0f339ac44a3
Rebeccasue91 <rebeccasue91@hotmail.com>
2026-06-25 12:28:49 -0700

    Update plants.js

--- plants.js
+++ plants.js
@@ -1,1 +1,26 @@
-
+const plants = [
+  {
+    id: "SG-104",
+    name: "Saguaro",
+    category: "Cactus",
+    price: 425,
+    qty: 3,
+    sun: "full",
+    water: "very low",
+    w: 10,
+    h: 35,
+    img: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=300"
+  },
+  {
+    id: "TS-201",
+    name: "Texas Sage",
+    category: "Shrub",
+    price: 38,
+    qty: 24,
+    sun: "full",
+    water: "low",
+    w: 5,
+    h: 5,
+    img: "https://images.unsplash.com/photo-1591857177580-dc82b9ac4e1e?w=300"
+  }
+];
