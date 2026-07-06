9d5ecea8e0df547bb3b60ff0fa60c4f775a881ea
Rebeccasue91 <rebeccasue91@hotmail.com>
2026-07-06 16:31:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 let selected = plants[0];
 let selectedPlacedId = null;
 let placed = [];
 let season = "summer";
 let growthYear = 0;
 
+function initApp() {
+  renderPlantList();
+  renderDetails();
+  renderGrid();
+}
 
-
-renderPlantList();
-renderDetails();
-renderGrid();
+initApp();
